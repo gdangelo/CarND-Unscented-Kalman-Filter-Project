0726c6f0eec09f7ad554df89271990044d9ec955
gdangelo <gregory@gdangelo.fr>
2018-02-16 11:27:26 -0500

    feat: Add percentage above threshold on graphs

--- plot_nis.py
+++ plot_nis.py
@@ -8,18 +8,29 @@ def read_file(filepath):
         data = str_data.split()
     return data
 
-def plot_graph(x, y, threshold, sensor_type):
-    plt.clf() # clear figure
+def plot_graph(x, y, threshold, sensor_type, percentage):
+    # Clear figure
+    plt.clf()
 
     fig = plt.figure()
 
+    # NIS values
+    plt.plot(x, y, color='b', label='NIS values at each step')
+    # In 5% of the time nis values should be higher than threshold
+    plt.axhline(y=threshold, color='r', label='threshold (' + str(threshold) + ')')
+
+    # Add axis labels
     plt.xlabel('step number')
     plt.ylabel('nis')
-    plt.title('Normalize Innovation Squared (NIS) graph')
 
-    plt.plot(x, y, color='b') # nis values
-    plt.axhline(y=threshold, color='r') # in 5% of the time nis values will be higher than threshold
+    # Add titles
+    title = 'Normalize Innovation Squared (NIS) graph'
+    subtitle = '{:.2f}'.format(percentage) + '% NIS values above threshold'
+    plt.title(title + '\n' + subtitle)
 
+    plt.legend(loc='upper right')
+
+    # Save graph
     if (sensor_type == 'L'):
         fig.savefig('nis_lidar_graph.png')
     elif (sensor_type == 'R'):
@@ -31,12 +42,18 @@ def main():
     nis_radar = read_file("NIS_Radar_.txt")
 
     # Store the values in a numpy array
-    nis_laser = np.array(nis_laser)
-    nis_radar = np.array(nis_radar)
+    nis_laser = np.array(nis_laser, dtype='f')
+    nis_radar = np.array(nis_radar, dtype='f')
+
+    # Find percentage above threshold
+    laser_threshold = 5.99
+    radar_threshold = 7.81
+    nis_above_laser_thres = len(np.where(nis_laser > laser_threshold)[0]) / float(len(nis_laser)) * 100
+    nis_above_radar_thres = len(np.where(nis_radar > radar_threshold)[0]) / float(len(nis_radar)) * 100
 
     # Plot the NIS graph using matplotlib
-    plot_graph(range(len(nis_laser)), nis_laser, 5.99, 'L')
-    plot_graph(range(len(nis_radar)), nis_radar, 7.81, 'R')
+    plot_graph(range(len(nis_laser)), nis_laser, laser_threshold, 'L', nis_above_laser_thres)
+    plot_graph(range(len(nis_radar)), nis_radar, radar_threshold, 'R', nis_above_radar_thres)
 
     print("Done.")
 
